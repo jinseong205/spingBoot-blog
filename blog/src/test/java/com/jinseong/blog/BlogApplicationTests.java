a0a152992338efa676077411bff20aa9e7446bea
jinseong <jinseong205@gmail.com>
2022-05-21 01:04:11 +0900

    Initial commit

---
+++ blog/src/test/java/com/jinseong/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinseong.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

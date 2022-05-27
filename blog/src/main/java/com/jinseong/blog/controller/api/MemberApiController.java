2a1bbeca7ec071893494d04bd1faa13bf2b4b739
jinseong <jinseong205@gmail.com>
2022-05-28 02:35:31 +0900

    ^^7

---
+++ blog/src/main/java/com/jinseong/blog/controller/api/MemberApiController.java
@@ -0,0 +1,5 @@
+package com.jinseong.blog.controller.api;
+
+public class MemberApiController {
+
+}
--- blog/src/main/resources/static/js/member.js
+++ blog/src/main/resources/static/js/member.js
@@ -1,7 +1,7 @@
 
 let index = {
 	init: function(){
-		$("#btn-save").on("click",() =>{
+		$("#btn-save").on("click",() =>{ // this 바인딩
 			this.save();
 		});
 	},	
@@ -11,10 +11,22 @@ let index = {
 			id: $("#id").val(),
 			password: $("#password").val(),
 			email: $("#email").val()
-		}
+		};
 		//console.log(data);
 		
-		$ajax().done().fail();	//ajax 통신을 이용해서 parameter를 json으로 변경하여 request
+		//ajax 통신을 이용해서 parameter를 json으로 변경하여 request
+		$ajax({
+			type:"POST",
+			url: "/api/member",
+			data: JSON.stringify(data), //http body data
+			contentType:"application/json; charset=utf-8", //request body dataType
+			dataType:"json" //response dataType
+		}).done(function(res){
+			alert("회원가입이 완료되었습니다.");
+			location.href="/blog";
+		}).fail(function(err){
+			alert(JSON.stringify(err))
+		});	
 	}
 }
 

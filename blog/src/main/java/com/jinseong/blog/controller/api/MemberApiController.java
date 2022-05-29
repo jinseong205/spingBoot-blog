852a5457472db87305b5e81fea5bde112d969ca2
jinseong <jinseong205@gmail.com>
2022-05-29 23:28:29 +0900

    ^^7

--- blog/src/main/java/com/jinseong/blog/controller/api/MemberApiController.java
+++ blog/src/main/java/com/jinseong/blog/controller/api/MemberApiController.java
@@ -1,5 +1,16 @@
 package com.jinseong.blog.controller.api;
 
-public class MemberApiController {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.jinseong.blog.model.Member;
+
+@RestController
+public class MemberApiController {
+	@PostMapping("/api/member")
+	public int save(@RequestBody Member member) {
+		System.out.println("save 호출됨");
+		return 1;
+	}
 }

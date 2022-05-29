309e8d34b849a4fb2acd320faea81532dc58e702
jinseong <jinseong205@gmail.com>
2022-05-30 01:15:24 +0900

    Update GlobalExceptionHanddler.java

--- blog/src/main/java/com/jinseong/blog/handler/GlobalExceptionHanddler.java
+++ blog/src/main/java/com/jinseong/blog/handler/GlobalExceptionHanddler.java
@@ -1,14 +1,17 @@
 package com.jinseong.blog.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jinseong.blog.dto.ResponseDto;
 
 @ControllerAdvice
 @RestController
 public class GlobalExceptionHanddler {
 	@ExceptionHandler(value=Exception.class)
-	public String hadnleArgumentException(Exception e) {
-		return "<h1>" + e.getMessage() + "</h1>";
+	public ResponseDto<String> hadnleArgumentException(Exception e) {
+		return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
 	}
 }

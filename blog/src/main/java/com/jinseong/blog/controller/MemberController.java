8c9fb198a05c5b9f7f008361cb66f75816646a77
jinseong <jinseong205@gmail.com>
2022-06-11 00:25:02 +0900

    ^^7

--- blog/src/main/java/com/jinseong/blog/controller/MemberController.java
+++ blog/src/main/java/com/jinseong/blog/controller/MemberController.java
@@ -1,9 +1,16 @@
 package com.jinseong.blog.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import com.jinseong.blog.auth.PrincipalDetail;
 
@@ -26,7 +33,33 @@ public class MemberController {
 	
 	@GetMapping("/auth/kakao/callback")
 	public @ResponseBody String kakaoLoginCallback(String code) { //Data를 리턴해주는 컨트롤러 함수
-		return "카카오 인증 완료 : " + code;
+		
+		//POST방식으로 key=value 데이터를 요청 (to 카카오)
+		RestTemplate rt = new RestTemplate();
+		
+		//Headers
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
+		
+		//Body
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "5e74706108bffe6ea2bef14ddb178495");
+		params.add("redirect_uri", "http://localhost:8000/auth/kakao/callback");
+		params.add("code", code);
+		
+		//Hedaer + Body
+		HttpEntity<MultiValueMap<String, String>> kakaoTokenReq = new HttpEntity<>(params,headers);
+		
+		//Http 요청하기 - Post방식 response 변수의 응답을 받음
+		ResponseEntity response = rt.exchange(
+				"https://kauth.kakao.com/oauth/token", 
+				HttpMethod.POST, 
+				kakaoTokenReq
+				,String.class
+			);
+		
+		return "카카오 인증 완료 : " + response;
 	}
 	
 	

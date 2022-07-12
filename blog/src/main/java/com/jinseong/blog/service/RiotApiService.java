fd4d87f2f742dc802804fd7b75cc6fd56a9b5977
jinseong <jinseong205@gmail.com>
2022-07-12 21:30:44 +0900

    restTemplate -> webClient

--- blog/src/main/java/com/jinseong/blog/service/RiotApiService.java
+++ blog/src/main/java/com/jinseong/blog/service/RiotApiService.java
@@ -1,13 +1,10 @@
 package com.jinseong.blog.service;
 
-
 import java.io.UnsupportedEncodingException;
 
 import org.apache.http.impl.client.HttpClients;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
@@ -18,11 +15,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import com.jinseong.blog.model.RiotInfo;
 
+import reactor.core.publisher.Mono;
+
 @Service
 public class RiotApiService {
-	
-	
-	
+
 	@Value("${riot.api.key}")
 	private String riotKey;
 
@@ -38,53 +35,69 @@ public class RiotApiService {
 	@Value("${riot.api.match-by-MatchId-url}")
 	private String matchByMatchIdurl;
 
-    
 	public String riotInfo(String username) throws Exception {
 
-		WebClient client = WebClient.create();	
+		// bodyData
+		// MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+
+		// WebClient client =
+		// WebClient.create("https://asia.api.riotgames.com/lol/match/v5/matches/KR_6008777525");
+
+		WebClient client = WebClient.create();
+
+		Mono<String> a = client.get().uri("https://asia.api.riotgames.com/lol/match/v5/matches/KR_6008777525").headers(headers -> {
+			headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+			headers.add("User-Agent",
+					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36");
+			headers.add("Accept-Charset", "application/x-www-form-urlencoded; charset=UTF-8");
+			headers.add("Origin", "https://developer.riotgames.com");
+			headers.add("X-Riot-Token", riotKey);
+		}).retrieve().bodyToMono(String.class);
 		
-		ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
-				HttpClients.createDefault());
-		RestTemplate rt = new RestTemplate(requestFactory);
-
-		HttpHeaders reqHeaders;
-		MultiValueMap<String, String> reqBody;
-
-		RiotInfo riotInfo = new RiotInfo();
-
-		/* Header + Body */
-		reqHeaders = new HttpHeaders();
-		reqHeaders.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-		reqHeaders.add("User-Agent",
-				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36");
-		reqHeaders.add("Accept-Charset", "application/x-www-form-urlencoded; charset=UTF-8");
-		reqHeaders.add("Accept-Language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7");
-		reqHeaders.add("Content-Encoding", "gzip");
-		reqHeaders.add("Origin", "https://developer.riotgames.com");
-		reqHeaders.add("X-Riot-Token", riotKey);
-
-		reqBody = new LinkedMultiValueMap<>();
-
-		HttpEntity<MultiValueMap<String, String>> HttpEntity = new HttpEntity<>(reqBody, reqHeaders);
-
+//		System.out.println(a);
+//		
+//		ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
+//				HttpClients.createDefault());
+//		RestTemplate rt = new RestTemplate(requestFactory);
+//
+//		HttpHeaders reqHeaders;
+//		MultiValueMap<String, String> reqBody;
+//
+//		RiotInfo riotInfo = new RiotInfo();
+//
+//		/* Header + Body */
+//		reqHeaders = new HttpHeaders();
+//		reqHeaders.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+//		reqHeaders.add("User-Agent",
+//				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36");
+//		reqHeaders.add("Accept-Charset", "application/x-www-form-urlencoded; charset=UTF-8");
+//		reqHeaders.add("Accept-Language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7");
+//		reqHeaders.add("Content-Encoding", "gzip");
+//		reqHeaders.add("Origin", "https://developer.riotgames.com");
+//		reqHeaders.add("X-Riot-Token", riotKey);
+//
+//		reqBody = new LinkedMultiValueMap<>();
+//
 //		HttpEntity<MultiValueMap<String, String>> HttpEntity = new HttpEntity<>(reqBody, reqHeaders);
-
-		byte[] bytes;
-		try {
-			bytes = "KR_6008777525".getBytes("UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			throw new UnsupportedEncodingException("인코딩에 실패하였습니다." + e.getMessage());
-		}
-		String matchGame = new String(bytes);
-
-		try {
-			System.out.println(matchGame);
-			 rt.exchange("https://asia.api.riotgames.com/lol/match/v5/matches/" + matchGame,
-					HttpMethod.GET, HttpEntity, String.class);
-			//System.out.println(a.toString());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//
+////		HttpEntity<MultiValueMap<String, String>> HttpEntity = new HttpEntity<>(reqBody, reqHeaders);
+//
+//		byte[] bytes;
+//		try {
+//			bytes = "KR_6008777525".getBytes("UTF-8");
+//		} catch (UnsupportedEncodingException e) {
+//			throw new UnsupportedEncodingException("인코딩에 실패하였습니다." + e.getMessage());
+//		}
+//		String matchGame = new String(bytes);
+//
+//		try {
+//			System.out.println(matchGame);
+//			rt.exchange("https://asia.api.riotgames.com/lol/match/v5/matches/" + matchGame, HttpMethod.GET, HttpEntity,
+//					String.class);
+//			// System.out.println(a.toString());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 
 //		/* Summoner */
 //
@@ -155,6 +168,6 @@ public class RiotApiService {
 //
 //		riotInfo.setMatchs(matchs);
 
-		return riotInfo.toString() + "<br/>";
+		return a.block() + "<br/>";
 	}
 }
